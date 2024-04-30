c5b4f5613a57c86fa30fff7bc2587ff7d480642f
Theresa Lang <theresa.lang@tngtech.com>
2024-04-30 18:07:40 +0200

    Fill in personal information

--- src/main.js
+++ src/main.js
@@ -4,25 +4,25 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Theresa Lang',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'Pipeline that includes automated buildiing, testing and deployment',
+			'Faster and more reliable releases'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'Jenkins',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'see and understand a complete CI/CD pipeline'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'sewing',
+			'running''
 		]
 	}
 });
